c020d44492ca1211ed414e65a6ed04e30edad1f6
lilin <2508063469@qq.com>
2020-03-21 17:42:51 +0800

    ...

--- Algorithms/Exercise/Chapter 1/Java/src/exercise.java
+++ Algorithms/Exercise/Chapter 1/Java/src/exercise.java
@@ -1,12 +1,16 @@
-public class excrise
+import sun.lwawt.macosx.CSystemTray;
+
+public class exercise
 {
     public static void main(String[] args)
     {
-        myFirstProgram();
+
     }
     /* test first program */
     public static void myFirstProgram()
     {
         System.out.println("hello word");
     }
+    // excrise
+    //public static void excrise_113
 }
