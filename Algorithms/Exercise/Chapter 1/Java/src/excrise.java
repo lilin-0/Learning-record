e308ac39af9cc7d49980f228946297e2d656cd15
lilin <2508063469@qq.com>
2020-03-21 17:28:24 +0800

    test

---
+++ Algorithms/Exercise/Chapter 1/Java/src/excrise.java
@@ -0,0 +1,12 @@
+public class excrise
+{
+    public static void main(String[] args)
+    {
+        myFirstProgram();
+    }
+    /* test first program */
+    public static void myFirstProgram()
+    {
+        System.out.println("hello word");
+    }
+}
