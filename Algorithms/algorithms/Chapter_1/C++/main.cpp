b64973151272168d71c386b9965937b3f43d5ecf
lilin <2508063469@qq.com>
2020-03-30 20:43:07 +0800

    提交练习

--- Algorithms/algorithms/Chapter_1/C++/main.cpp
+++ Algorithms/algorithms/Chapter_1/C++/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <queue>
 #include "Queue.h"
+#include "UF.h"
 
 using namespace std;
+void test_UF();
 
-int main() {
+int main()
+{
+    test_UF();
+    return 0;
+}
+
+void test_queue()
+{
     Queue<int> t;
     t.push(33);
     t.push(22);
@@ -17,5 +26,13 @@ int main() {
     t.push(332);
     cout<<t.pop(w)<<' '<<w<<endl;
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+}
+void test_UF()
+{
+    UF test(10);
+    int a,b;
+    while(cin>>a>>b)
+        test.quick_union(a,b);
+    for(int i = 0; i < 10; i++)
+        cout<<test.find(i)<<endl;
 }
